78f5ce6449732af2a5c0c6582098ed8eae5d0956
nghiepvo <vokenghiep@gmail.com>
2017-12-06 15:32:42 +0700

    commit NLog

---
+++ ConsoleExample/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace ConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var servicesProvider = BuildDi();
+            var runner = servicesProvider.GetRequiredService<Runner>();
+
+            runner.DoAction("Action1");
+
+            Console.WriteLine("Press ANY key to exit");
+            Console.ReadLine();
+        }
+
+
+        private static IServiceProvider BuildDi()
+        {
+            var services = new ServiceCollection();
+
+            //Runner is the custom class
+            services.AddTransient<Runner>();
+
+            services.AddSingleton<ILoggerFactory, LoggerFactory>();
+            services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+            services.AddLogging((builder) => builder.SetMinimumLevel(LogLevel.Trace));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+            //configure NLog
+            loggerFactory.AddNLog(new NLogProviderOptions { CaptureMessageTemplates = true, CaptureMessageProperties = true });
+            loggerFactory.ConfigureNLog("nlog.config");
+
+            return serviceProvider;
+        }
+    }
+
+
+    public class Runner
+    {
+        private readonly ILogger<Runner> _logger;
+
+        public Runner(ILogger<Runner> logger)
+        {
+            _logger = logger;
+        }
+
+        public void DoAction(string name)
+        {
+            _logger.LogDebug(20, "Doing hard work! {Action}", name);
+            var a = 10;
+            var b = 0;
+            try
+            {
+                a = a / b;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "this is a test");
+            }
+            
+        }
+
+
+    }
+}
--- RedisDemo/Program.cs
+++ RedisDemo/Program.cs
@@ -1,5 +1,7 @@
-﻿
+﻿using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
 using System;
 using System.Text;
 using System.Threading;
@@ -13,24 +15,44 @@ namespace RedisDemo
         public string Name { get; set; }
         public DateTimeOffset CreatedDate { get; set; }
     }
+
+    
     class Program
     {
         const int PublishMessageCount = 5;
         const string ChannelName = "SimplePubSubCHANNEL";
         const string MessagePrefix = "MESSAGE ";
+        
         static void Main(string[] args)
         {
+            var loggerDebugFactory = new LoggerFactory();
+            loggerDebugFactory.AddNLog(new NLogProviderOptions { CaptureMessageTemplates = true, CaptureMessageProperties = true });
+            loggerDebugFactory.ConfigureNLog("nlog.config");
+            ILogger<Program> _logger = loggerDebugFactory.CreateLogger<Program>();
+            _logger.LogDebug(20, "Doing hard work! {Action}", "abc");
+            var a = 10;
+            var b = 0;
+            try
+            {
+                a = a / b;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "this is a test");
+            }
+
             var messagesReceived = 0;           
             
             using (var client = new RedisClient("localhost", 6379, "12345^"))
             {
-                
+                // redis type string.
                 client.Set("student1", new Student
                 {
                     Id = 1,
                     Name = "Nghiep",
                     CreatedDate = DateTimeOffset.Now
                 });
+                // redis type hash.
                 client.StoreAsHash<Student>(new Student
                 {
                     Id = 4,
@@ -39,21 +61,32 @@ namespace RedisDemo
                 });
 
                 var fromHash = client.GetFromHash<Student>(1);
+                if (fromHash.Id != 0)
+                {
+                    //Exited in cache
+                }
+
+                // redis type list.
+                var redis = client.As<Student>();
+                var studentKey = string.Format("student:id:{0}", 1);
+                var student = new Student { Id = 1, Name = "Nghiep", CreatedDate = DateTimeOffset.Now};
+                redis.SetValue(studentKey, student);
+
                 var student1 = client.Get<Student>("student1");
 
                 using (var subscription = client.CreateSubscription())
                 {
                     subscription.OnSubscribe = channel =>
                     {
-                        Console.WriteLine("Subscribed to '{0}'", channel);
+                        _logger.LogDebug(string.Format("Subscribed to '{0}'", channel));
                     };
                     subscription.OnUnSubscribe = channel =>
                     {
-                        Console.WriteLine("UnSubscribed from '{0}'", channel);
+                        _logger.LogDebug(string.Format("UnSubscribed from '{0}'", channel));
                     };
                     subscription.OnMessage = (channel, msg) =>
                     {
-                        Console.WriteLine("Received '{0}' from channel '{1}'", msg, channel);
+                        _logger.LogDebug(string.Format("Received '{0}' from channel '{1}'", msg, channel));
                         
                         //As soon as we've received all 5 messages, disconnect by unsubscribing to all channels
                         if (++messagesReceived == PublishMessageCount)
@@ -65,21 +98,21 @@ namespace RedisDemo
                     ThreadPool.QueueUserWorkItem(x =>
                     {
                         Thread.Sleep(200);
-                        Console.WriteLine("Begin publishing messages...");
+                        _logger.LogDebug("Begin publishing messages...");
 
                         using (var redisPublisher = new RedisClient("localhost", 6379, "12345^"))
                         {
                             for (var i = 1; i <= PublishMessageCount; i++)
                             {
                                 var message = MessagePrefix + i;
-                                Console.WriteLine("Publishing '{0}' to '{1}'", message, ChannelName);
+                                _logger.LogDebug(string.Format("Publishing '{0}' to '{1}'", message, ChannelName));
                                 redisPublisher.PublishMessage(ChannelName, message);
                                 Thread.Sleep(2000);
                             }
                         }
                     });
 
-                    Console.WriteLine("Started Listening On '{0}'", ChannelName);
+                    _logger.LogDebug(string.Format("Started Listening On '{0}'", ChannelName));
                     subscription.SubscribeToChannels(ChannelName); //blocking
                 }
             }
